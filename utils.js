b94bff0d7684137cda1d93403c87d6ce3a6c8edc
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-08-12 20:45:35 -0500

    first commit

---
+++ utils.js
@@ -0,0 +1,12 @@
+const debounce = (func, delay=1000) => {
+   let timeOutId;
+   return (...args) => {
+      if (timeOutId) {
+         clearTimeout(timeOutId)
+      }
+      timeOutId = setTimeout(() => {
+         func.apply(null, args)
+      }, delay)
+   }
+}
+
